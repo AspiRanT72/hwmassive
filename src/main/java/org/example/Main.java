6d41fe2fe9f41ee40eea519657c66fe0fd007547
AspiRanT <123123zqwer72@gmail.com>
2023-02-05 18:40:29 +0500

    Half end

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+
+import org.example.lessoner.Task;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Task task = new Task();
+        task.First();
+        task.Second();
+        task.Third();
     }
 }
---
+++ src/main/java/org/example/lessoner/Task.java
@@ -0,0 +1,45 @@
+package org.example.lessoner;
+
+public class Task {
+    public void First() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] *= 2;
+            }
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void Second() {
+        int[][] arr = new int[5][5];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (i == j) {
+                    arr[i][j] = 1;
+                }
+                arr[i][arr[i].length - 1 - i] = 1;
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    //Необходимо задать одномерный массив и найти в нем минимальный и максимальный элементы
+    public void Third() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Минимальный элемент массива: " + min);
+        System.out.println("Максимальный элемент массива: " + max);
+    }
+
+}
